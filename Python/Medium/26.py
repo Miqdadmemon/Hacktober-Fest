6932a1cf19cffc4375e22706622d12107f41a30b
Miqdadmemon <miqdadm15@gmail.com>
2024-10-05 13:49:47 +0530

    26.py
    python

--- Python/Medium/26.py
+++ Python/Medium/26.py
@@ -1,10 +1,10 @@
-def multiply:
-    Return a*b
-a=int(input(‘enter a num’))
-b=int(input(‘enter another number’))
-
+a=int(input("enter a num"))
+b=int(input("enter another number"))
+def multiply(a,b):
+    return a*b
+A=multiply(a,b)
+print(A)
 def square(num):
+    print(num*num)
     return num * num
-
-
-square  
+square(5) 
