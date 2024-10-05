a6a39fc9d2354ac7a04eef4e7b72a7234a02eb18
Miqdadmemon <miqdadm15@gmail.com>
2024-10-05 12:39:01 +0530

    python
    que 1 easy

--- Python/Easy/1.py
+++ Python/Easy/1.py
@@ -1,9 +1,10 @@
-If x==10
+x=int(input("enter number 1"))
+y=int(input("enter your no1 : "))
+if x==10:
 	print(x)
-—------------------
 
-if y > 5
+if y > 5:
     print(y)
 
-for i in range(5)
+for i in range(5):
     print(i)
